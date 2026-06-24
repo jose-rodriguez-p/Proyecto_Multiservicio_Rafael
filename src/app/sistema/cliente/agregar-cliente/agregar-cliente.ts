12ac76897494cc543b33e844006964a4be15a380
ZegarraRobert <robertzegarralima2210@gmail.com>
2026-06-24 00:33:06 -0500

    Editar modals en detalles del vehiculo

--- src/app/sistema/cliente/agregar-cliente/agregar-cliente.ts
+++ src/app/sistema/cliente/agregar-cliente/agregar-cliente.ts
@@ -31,6 +31,78 @@ export class AgregarCliente {
   errorCelular = false;
   errorCorreo = false;
   permiteEdicionManual = false;
+  errorPlaca = false;
+
+  readonly catalogoMarcas: Record<string, string[]> = {
+    'Toyota':        ['Avanza', 'Camry', 'Corolla', 'FJ Cruiser', 'Fortuner', 'Hiace', 'Hilux', 'Innova', 'Land Cruiser', 'Land Cruiser Prado', 'RAV4', 'Rush', 'Yaris'],
+    'Hyundai':       ['Accent', 'Creta', 'Elantra', 'Grand i10', 'H1', 'HD65', 'HD78', 'Ioniq 5', 'Kona', 'Santa Fe', 'Sonata', 'Tucson'],
+    'Kia':           ['Carnival', 'Cerato', 'EV6', 'Grand Carnival', 'K2500', 'Niro', 'Picanto', 'Rio', 'Seltos', 'Sorento', 'Sportage', 'Stinger'],
+    'Nissan':        ['Frontier', 'Kicks', 'Leaf', 'Murano', 'Navara', 'NP300', 'Pathfinder', 'Qashqai', 'Sentra', 'Terra', 'Versa', 'X-Trail'],
+    'Chevrolet':     ['Aveo', 'Blazer', 'Captiva', 'Colorado', 'Cruze', 'D-Max', 'Equinox', 'N300', 'Silverado', 'Spark', 'Tracker', 'Trailblazer'],
+    'Mitsubishi':    ['ASX', 'Eclipse Cross', 'Galant', 'L200', 'Lancer', 'Mirage', 'Montero', 'Montero Sport', 'Outlander', 'Pajero', 'Xpander'],
+    'Ford':          ['Bronco', 'EcoSport', 'Edge', 'Escape', 'Explorer', 'F-150', 'F-250', 'Mustang', 'Ranger', 'Territory'],
+    'Honda':         ['Accord', 'BR-V', 'City', 'Civic', 'CR-V', 'Fit', 'HR-V', 'Jazz', 'Odyssey', 'Pilot'],
+    'Suzuki':        ['Alto', 'Baleno', 'Ertiga', 'Grand Vitara', 'Ignis', 'Jimny', 'S-Cross', 'Swift', 'Vitara'],
+    'Mazda':         ['BT-50', 'CX-3', 'CX-30', 'CX-5', 'CX-9', 'Mazda 2', 'Mazda 3', 'Mazda 6', 'MX-5'],
+    'Volkswagen':    ['Amarok', 'Golf', 'Jetta', 'Passat', 'Polo', 'T-Cross', 'Taos', 'Tiguan', 'Touareg', 'Virtus'],
+    'Renault':       ['Captur', 'Clio', 'Duster', 'Kangoo', 'Koleos', 'Logan', 'Oroch', 'Sandero', 'Stepway'],
+    'Peugeot':       ['208', '2008', '3008', '5008', '508', 'Boxer', 'Expert', 'Partner'],
+    'Fiat':          ['Cronos', 'Ducato', 'Fiorino', 'Mobi', 'Pulse', 'Strada', 'Toro'],
+    'Subaru':        ['Forester', 'Impreza', 'Legacy', 'Outback', 'WRX', 'XV'],
+    'Jeep':          ['Cherokee', 'Compass', 'Gladiator', 'Grand Cherokee', 'Renegade', 'Wrangler'],
+    'BMW':           ['Serie 1', 'Serie 2', 'Serie 3', 'Serie 5', 'Serie 7', 'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7'],
+    'Mercedes-Benz': ['Clase A', 'Clase B', 'Clase C', 'Clase E', 'Clase S', 'GLA', 'GLB', 'GLC', 'GLE', 'GLK', 'GLS', 'Sprinter', 'Vito'],
+    'Audi':          ['A1', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'Q2', 'Q3', 'Q5', 'Q7', 'Q8', 'TT'],
+    'Chery':         ['Arrizo 5', 'Arrizo 6', 'Omoda 5', 'Tiggo 2', 'Tiggo 2 Pro', 'Tiggo 4', 'Tiggo 4 Pro', 'Tiggo 7', 'Tiggo 7 Pro', 'Tiggo 8', 'Tiggo 8 Pro'],
+    'JAC':           ['J4', 'J7', 'S2', 'S3', 'S4', 'T6', 'T8', 'X200', 'X350'],
+    'DFSK':          ['C31', 'C35', 'Glory 330', 'Glory 360', 'Glory 500', 'Glory 580', 'K01H', 'K07'],
+    'Great Wall':    ['Haval H6', 'Haval Jolion', 'ORA 03', 'Poer', 'Wingle 5', 'Wingle 6', 'Wingle 7'],
+    'MG':            ['HS', 'MG 3', 'MG 5', 'MG 6', 'MG ZS', 'RX5', 'ZS EV'],
+    'BYD':           ['Atto 3', 'Dolphin', 'F3', 'Han', 'King', 'Plus', 'Seal', 'Song Plus', 'Tan'],
+    'Isuzu':         ['D-Max', 'ELF 150', 'ELF 250', 'ELF 300', 'FRR', 'FTR', 'MU-X', 'NKR', 'NLR', 'NPR'],
+    'Hino':          ['300 Series', '500 Series', '700 Series', 'Dutro', 'Ranger'],
+    'Volvo':         ['S60', 'S90', 'V60', 'XC40', 'XC60', 'XC90'],
+    'Land Rover':    ['Defender', 'Discovery', 'Discovery Sport', 'Freelander', 'Range Rover', 'Range Rover Evoque', 'Range Rover Sport'],
+    'Geely':         ['Azkarra', 'Coolray', 'Emgrand', 'GX3 Pro', 'Okavango', 'Tugella'],
+    'Changan':       ['CS15', 'CS35 Plus', 'CS55 Plus', 'CS75 Plus', 'Hunter', 'Oshan X5', 'Uni-K', 'Uni-T'],
+    'GAC':           ['Empow', 'GN8', 'GS3', 'GS4', 'GS5', 'GS8', 'M6'],
+  };
+
+  get marcas(): string[] {
+    return Object.keys(this.catalogoMarcas).sort();
+  }
+
+  get modelosFiltrados(): string[] {
+    return this.nuevoVehiculo.marca
+      ? (this.catalogoMarcas[this.nuevoVehiculo.marca] ?? []).sort()
+      : [];
+  }
+
+  get anios(): number[] {
+    const actual = new Date().getFullYear();
+    const lista: number[] = [];
+    for (let a = actual; a >= 1990; a--) lista.push(a);
+    return lista;
+  }
+
+  onMarcaChange() {
+    this.nuevoVehiculo.modelo = '';
+    this.nuevoVehiculo.anio = '';
+  }
+
+  onModeloChange() {
+    this.nuevoVehiculo.anio = '';
+  }
+
+  formatearPlaca() {
+    let v = this.nuevoVehiculo.placa.toUpperCase().replace(/[^A-Z0-9]/g, '');
+    if (v.length > 3) v = v.slice(0, 3) + '-' + v.slice(3, 7);
+    this.nuevoVehiculo.placa = v;
+    const valido = /^[A-Z]{3}-\d{3}$/.test(v) || /^[A-Z]{2}-\d{4}$/.test(v);
+    this.errorPlaca = v.replace('-', '').length >= 5 && !valido;
+  }
+
+
 
   validarDni() {
     const dni = this.nuevoCliente.dni.replace(/\D/g, '');
